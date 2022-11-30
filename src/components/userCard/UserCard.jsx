d97c772bd8853e41a06296118c80c09cdea913d7
Marius <marius@example.com>
2022-11-30 09:23:24 +0200

    user card styles

--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -1,16 +1,16 @@
 // 1. itraukti modulini css
-
+import css from './UserCard.module.css';
 // 2. css aprasyti klases .card, .title, .text priduodami kazkokio grazaus stiliaus.
 
 // 3. panaudojam modulini css
 
 function UserCard(props) {
   return (
-    <div className='card'>
+    <div className={css.card}>
       <img src='' alt='user' />
-      <h2 className='title'>Name</h2>
+      <h2 className={css.title}>Name</h2>
       <h3>age</h3>
-      <p className='text'>
+      <p className={css.text}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, facilis.
       </p>
     </div>
