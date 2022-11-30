3058fab69fc58c593b10c37a8e8a2b37ec37ca09
Marius <marius@example.com>
2022-11-30 08:42:39 +0200

    css bleed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className='App container'>
       <h1>React</h1>
+
       <Card />
+
+      <div className='card'>
+        <h2 className='title'>Hello</h2>
+      </div>
     </div>
   );
 }
