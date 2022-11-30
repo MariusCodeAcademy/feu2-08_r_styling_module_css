c12282abefe6a396b76482f41e29a6c77e7b3999
Marius <marius@example.com>
2022-11-30 08:53:29 +0200

    cardModule styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,12 @@
-import './Card.css';
+import css from './Card.module.css';
 
 function Card(props) {
+  // console.log('css ===', css);
   return (
-    <div className='card'>
-      <h2 className='title'>James Bond</h2>
-      <h3 className='subtitle'>Title: 007</h3>
-      <p>
+    <div className={css.card}>
+      <h2 className={css.title}>James Bond</h2>
+      <h3 className={css.subtitle}>Title: 007</h3>
+      <p className={css.text}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
         possimus accusamus tempore veniam, a assumenda quo nisi rem repellendus
         numquam!
