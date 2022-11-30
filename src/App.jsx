9655f06d25a9d220f8f363cd0710c02705753ceb
Marius <marius@example.com>
2022-11-30 10:31:12 +0200

    button css

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Card from './components/card/Card';
 import UserCard from './components/userCard/UserCard';
 import Grid from './components/UI/grid/Grid';
+import Button from './components/UI/btn/Button';
 
 function App() {
   return (
     <div className='App container'>
       <h1>React</h1>
+      <Button>Read more</Button>
 
       <Grid cols={2}>
         <Card />
---
+++ src/components/UI/btn/Button.jsx
@@ -0,0 +1,6 @@
+import css from './Button.module.css';
+
+function Button(props) {
+  return <button className={css.btn}>Button</button>;
+}
+export default Button;
