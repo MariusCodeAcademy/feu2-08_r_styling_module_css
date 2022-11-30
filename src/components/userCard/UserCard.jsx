a5b8dbbd93a2c2de789cec39ee67b9f40f25ad9b
Marius <marius@example.com>
2022-11-30 09:45:44 +0200

    userData card

--- src/components/userCard/UserCard.jsx
+++ src/components/userCard/UserCard.jsx
@@ -1,18 +1,36 @@
 // 1. itraukti modulini css
+import { useEffect, useState } from 'react';
 import css from './UserCard.module.css';
 // 2. css aprasyti klases .card, .title, .text priduodami kazkokio grazaus stiliaus.
 
 // 3. panaudojam modulini css
 
 function UserCard(props) {
+  const [userData, setUserData] = useState({});
+
+  // efektas kuris pasileidzia tik susikurus komponentui
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  async function getUser() {
+    const url = 'https://dummyjson.com/users/1';
+    const resp = await fetch(url);
+    const dataInJs = await resp.json();
+    // gavom duomenis
+    console.log('dataInJs ===', dataInJs);
+    setUserData(dataInJs);
+  }
+
+  const { image, lastName, firstName, age, height } = userData;
   return (
     <div className={css.card}>
-      <img src='' alt='user' />
-      <h2 className={css.title}>Name</h2>
-      <h3>age</h3>
-      <p className={css.text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, facilis.
-      </p>
+      <img src={image} alt='user' />
+      <h2 className={css.title}>
+        {firstName} {lastName}
+      </h2>
+      <h3>Age {age}</h3>
+      <p className={css.text}>Height: {height}cm</p>
     </div>
   );
 }
